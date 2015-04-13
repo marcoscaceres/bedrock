c8f5a1009fcae2a102daf59f7c5afbcd35776c99
Craig Cook <craigcook@gmail.com>
2015-04-13 00:33:16 -0700

    Bug 1136436 - Firefox for iOS product page

---
+++ media/js/firefox/ios.js
@@ -0,0 +1,146 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+* License, v. 2.0. If a copy of the MPL was not distributed with this
+* file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+// create namespace
+if (typeof window.Mozilla === 'undefined') {
+    window.Mozilla = {};
+}
+
+;(function($, Mozilla) {
+    'use strict';
+
+    var $body = $('body');
+    var $html = $('html');
+    var COUNTRY_CODE = '';
+
+    // Countries in which Firefox for iOS is available.
+    // Add two-letter country codes (lowercase) to this array to
+    // to expand the market. We'll remove this logic for wide release.
+    // Note: This is the country code, NOT locale code; they often differ.
+    var marketCountries = ['nz'];
+
+    // Get country code via geo-ip lookup
+    var getGeoLocation = function() {
+        try {
+            COUNTRY_CODE = geoip_country_code().toLowerCase();
+        } catch (e) {
+            COUNTRY_CODE = '';
+        }
+    };
+
+    $.getScript('https://geo.mozilla.org/country.js', function() {
+        getGeoLocation();
+
+        for (var i = 0; i < marketCountries.length; i++) {
+            if (marketCountries[i].match(COUNTRY_CODE)) {
+                $body.addClass('in-market');
+            }
+        }
+    });
+
+    // init send-to-device form
+    var form = new Mozilla.SendToDevice();
+    form.init();
+
+    var $widget = $('#send-to-modal-container');
+
+    $('.send-to').on('click', function(e) {
+        e.preventDefault();
+        Mozilla.Modal.createModal(this, $widget);
+    });
+
+    // Firefox Sync sign in flow button
+    $('.sync-button').on('click', function(e) {
+        e.preventDefault();
+        Mozilla.UITour.showFirefoxAccounts();
+    });
+
+    // Show Sync instructions in a modal doorhanger
+    var $instructions = $('#sync-instructions');
+    var $fill = $('<div id="modal" role="dialog" tabindex="-1"></div>');
+
+    $('.sync-start-ios').on('click', function(e) {
+        e.preventDefault();
+        $html.addClass('noscroll');
+        $fill.append($instructions);
+        $body.append($fill);
+    });
+
+    $('#sync-instructions .btn-dismiss').on('click', function(e) {
+        e.preventDefault();
+        $html.removeClass('noscroll');
+        $body.append($instructions);
+        $fill.remove();
+    });
+
+    // Sync stuff
+    var fxMasterVersion = window.getFirefoxMasterVersion();
+    var state = 'Unknown';
+    var syncCapable = false;
+
+    var swapState = function(stateClass) {
+        $body.removeClass('state-default');
+        $body.addClass(stateClass);
+    };
+
+    // Sniff UA for Firefox for iOS
+    var isFirefoxiOS = function(userAgent) {
+        var ua = userAgent || navigator.userAgent;
+        return /FxiOS/.test(ua);
+    };
+
+    if (window.isFirefox()) {
+        // Firefox for Android
+        if (window.isFirefoxMobile()) {
+
+            swapState('state-fx-android');
+            state = 'Firefox for Android';
+
+        // Firefox for Desktop
+        } else {
+
+            if (fxMasterVersion >= 31) {
+
+                // Set syncCapable so we know not to send tracking info
+                // again later
+                syncCapable = true;
+
+                // Query if the UITour API is working before we use the API
+                Mozilla.UITour.getConfiguration('sync', function (config) {
+
+                    // Variation #1: Firefox 31+ signed IN to Sync (default)
+                    if (config.setup) {
+
+                        swapState('state-fx-signed-in');
+                        state = 'Firefox 31 or Higher: Signed-In';
+
+                    // Variation #2: Firefox 31+ signed OUT of Sync
+                    } else {
+                        swapState('state-fx-signed-out');
+                        state = 'Firefox 31 or Higher: Signed-Out';
+                    }
+
+                    // Call GA tracking here to ensure it waits for the
+                    // getConfiguration async call
+                    window.dataLayer.push({
+                        event: 'page-load',
+                        browser: state
+                    });
+                });
+            }
+
+        }
+
+    // Firefox for iOS
+    } else if (isFirefoxiOS()) {
+        swapState('state-fx-ios');
+        state = 'Firefox for iOS';
+
+    // Not Firefox
+    } else {
+        swapState('state-not-fx');
+        state = 'Not Firefox';
+    }
+
+})(window.jQuery, window.Mozilla);
